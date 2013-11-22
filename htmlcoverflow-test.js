40810228c5186b4ce66c1e817b49799cd200808a
rasystems <vanroman@web.de>
2013-11-22 08:53:08 +0100

    Delete htmlcoverflow-test.js

--- htmlcoverflow-test.js
+++
@@ -1,6 +0,0 @@
-define(['testsuite/qunit', 'jquery', 'ep/ui/htmlcoverflow', 'ep/ui/validate', 'ep/sprite', '$ready!'], function (QUnit, $, ep) {
-
-	QUnit.module("ep/ui/dialog");
-
-	$('#epslider-wrap').uiHtmlcoverflow({});
-});
